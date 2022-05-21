e988646c106e5e00e1fe27475b4497d2c25b1152
gitJaesik <maguire1815@gmail.com>
2022-05-21 22:46:29 +0900

    Update comment and apply autopep8

--- quickstart.py
+++ quickstart.py
@@ -8,7 +8,6 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 
-
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
@@ -26,9 +25,10 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
+
 class ConvNet(nn.Module):
     def __init__(self):
-        super(ConvNet,self).__init__()
+        super(ConvNet, self).__init__()
         self.layer1 = nn.Sequential(
             # x input height = 28
             # x inpurt width = 28
@@ -36,7 +36,8 @@ class ConvNet(nn.Module):
             # = (28 + 2*2 -5)/1 + 1 = 28
             # output_width = (Width + 2*padding - FilterHeight)/Stride + 1
             # =
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(in_channels=1, out_channels=32,
+                      kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
             # output_height =(Height + 2*padding - dilation[0] * (kernel_size[0]-1) -1) / stride[0] +1
             # output_width = (Width + 2*padding - dilation[1] * (kernel_size[1]-1) -1) / stride[1] +1
@@ -45,7 +46,8 @@ class ConvNet(nn.Module):
         )
 
         self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(in_channels=32, out_channels=64,
+                      kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
@@ -53,11 +55,11 @@ class ConvNet(nn.Module):
         self.fc1 = nn.Linear(in_features=7*7*64, out_features=1000)
         self.fc2 = nn.Linear(in_features=1000, out_features=10)
 
-    def forward(self,x):
+    def forward(self, x):
         # x = 1x28x28 input
         x1 = self.layer1(x)
         x2 = self.layer2(x1)
-        x3 = x2.reshape(x2.size(0),-1)
+        x3 = x2.reshape(x2.size(0), -1)
         x4 = self.dropout(x3)
         x5 = self.fc1(x4)
         x6 = self.fc2(x5)
@@ -66,20 +68,22 @@ class ConvNet(nn.Module):
 
 class PytorchTest:
     def __init__(self,
-                 training_data:dataset,
-                 test_data:dataset,
-                 batch_size:int,
-                 module:nn.Module,
-                 loss_fn:nn.Module,
-                 optimizer:optimizer,
+                 training_data: dataset,
+                 test_data: dataset,
+                 batch_size: int,
+                 module: nn.Module,
+                 loss_fn: nn.Module,
+                 optimizer: optimizer,
                  ):
         self.batch_size = batch_size
         self.training_data = training_data
         self.test_data = test_data
-        self.train_dataloader = DataLoader(self.training_data, batch_size=self.batch_size)
-        self.test_dataloader = DataLoader(self.test_data, batch_size=self.batch_size)
+        self.train_dataloader = DataLoader(
+            self.training_data, batch_size=self.batch_size)
+        self.test_dataloader = DataLoader(
+            self.test_data, batch_size=self.batch_size)
 
-        for x,y in self.test_dataloader:
+        for x, y in self.test_dataloader:
             print(f"Shape of X [Number, Color, Height, Width]: {x.shape}")
             print(f"Shape of y: {y.shape} {y.dtype}")
             break
@@ -93,7 +97,6 @@ class PytorchTest:
         self.loss_fn = loss_fn
         self.optimizer = optimizer(self.model.parameters(), lr=1e-3)
 
-
     def train(self):
         size = len(self.train_dataloader.dataset)
         self.model.train()
@@ -104,16 +107,16 @@ class PytorchTest:
 
             # Compute prediction error
             pred = self.model(X)
-            loss = self.loss_fn(pred, y) #shape 확인. pred, y
-            #loss의 shape확인,
+            loss = self.loss_fn(pred, y)  # shape 확인. pred, y
+            # loss의 shape확인,
 
             # Backpropagation
-            self.optimizer.zero_grad() #다음시간에.
+            self.optimizer.zero_grad()  # 다음시간에.
             loss.backward()
-            self.optimizer.step() #다음시간에.
+            self.optimizer.step()  # 다음시간에.
 
             if batch % 100 == 0:
-                #loss가 1차원 값으로 어떻게 계산되는지 확인.
+                # loss가 1차원 값으로 어떻게 계산되는지 확인.
                 loss, current = loss.item(), batch * len(X)
                 print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
@@ -130,13 +133,20 @@ class PytorchTest:
                 correct += (pred.argmax(1) == y).type(torch.float).sum().item()
         test_loss /= num_batches
         correct /= size
-        print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+        print(
+            f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-    def run(self,epochs:int):
+    def run(self, epochs: int):
         for t in range(epochs):
             print(f"Epoch {t + 1}\n-------------------------------")
             self.train()
             self.test()
+            # tensorboard
+            # model save
+            # early stopping 가장 근본은 정규화, 과적합을 방지
+            # validation loss는 줄어들지 않음, trainset loss는 계속 줄어듬
+            # model early stopping ? break
+
         print("Done!")
 
     def save(self):
@@ -172,8 +182,11 @@ class PytorchTest:
             predicted, actual = classes[pred[0].argmax(0)], classes[y]
             print(f'Predicted: "{predicted}", Actual: "{actual}"')
 
+
 if __name__ == '__main__':
+    # 10
     mnist_train = datasets.FashionMNIST(
+        # magic string
         root='data',
         train=True,
         download=True,
@@ -191,9 +204,10 @@ if __name__ == '__main__':
     #     pt.load()
 
     module = ConvNet()
-    pt = PytorchTest(mnist_train,mnist_test,64, module, nn.CrossEntropyLoss(), torch.optim.Adam)
-
+    # magic number
+    pt = PytorchTest(mnist_train, mnist_test, 64, module,
+                     nn.CrossEntropyLoss(), torch.optim.Adam)
 
     pt.run(5)
-    #pt.save()
+    # pt.save()
     pt.predict()
